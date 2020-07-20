3c7445cc3b9da6e911b74f7b75d72db9b9d809fd
Jhonny <jhonny.sicol@gmail.com>
2020-07-20 13:57:33 -0300

    test

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  const token = core.getInput('github-oauth-token');
+  const { GITHUB_REPOSITORY, GITHUB_SHA } = process.env;
+  console.log({ token, GITHUB_REPOSITORY, GITHUB_SHA })
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
+
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
